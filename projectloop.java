18b686c44cef74dcd5854cedb1d5c53494ba3311
sclarke28 <beaunelle@aol.com>
2022-10-06 19:44:13 -0400

    Loop

---
+++ projectloop.java
@@ -0,0 +1,12 @@
+package classlab;
+
+public class projectloop {
+
+	public static void main(String[] args) {
+		int num = 7;
+		for(int i = 1; i <= 10; ++i)
+		{	
+			System.out.printf("%d * %d = %d /n" , num,i, num *1);
+		}
+	}
+}
